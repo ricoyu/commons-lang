b6c406ce61207837c2c6822d31564ee6cad4d7eb
Star Lord <ricoyu520@gmail.com>
2019-12-25 11:55:00 +0800

    增加HttpUtils

---
+++ src/main/java/com/loserico/common/lang/utils/NetworkUtils.java
@@ -0,0 +1,17 @@
+package com.loserico.common.lang.utils;
+
+/**
+ * HTTP, TCP 等等网络相关操作的工具类
+ * <p>
+ * Copyright: (C), 2019/12/25 10:24
+ * <p>
+ * <p>
+ * Company: Sexy Uncle Inc.
+ *
+ * @author Rico Yu ricoyu520@gmail.com
+ * @version 1.0
+ */
+public final class NetworkUtils {
+	
+	
+}
